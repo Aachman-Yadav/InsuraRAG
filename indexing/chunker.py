3b6899ae5c0f03f5aade6dd4a936e69bae5f682c
Nishchal Chandel <nishchandel21@gmail.com>
2025-07-24 21:50:28 +0530

    Added chunker to split text into small chunks

---
+++ indexing/chunker.py
@@ -0,0 +1,32 @@
+######################
+# Chunker
+######################
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from utils.generate_uid import generate_chunk_uid
+from utils.logger import logger
+
+def chunk_documents(documents, chunk_size=5000, chunk_overlap=500):
+    """Splits loaded LangChain documents into smaller, overlapping chunks using RecursiveCharacterTextSplitter."""
+    
+    logger.info(f"Starting chunking: {len(documents)} documents, chunk_size={chunk_size}, overlap={chunk_overlap}")
+    
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, 
+        chunk_overlap=chunk_overlap, 
+        separators=["\n\n", "\n", ".", " "]
+    )
+    
+    chunked_docs = splitter.split_documents(documents)
+    logger.info(f"Chunking complete: {len(chunked_docs)} total chunks generated.")
+    
+    for idx, chunk in enumerate(chunked_docs):
+        
+        doc_uid = chunk.metadata.get("doc_uid", "UNKNOWN")
+                
+        chunk.metadata["chunk_index"] = idx
+        chunk.metadata["chunk_size"] = len(chunk.page_content)
+        chunk.metadata["chunk_uid"] = generate_chunk_uid(doc_uid, idx)
+        chunk.metadata["doc_uid"] = doc_uid
+        
+    return chunked_docs
